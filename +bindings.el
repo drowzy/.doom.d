c7039c11b9769a9feafaca9bd40c89dfa734e17c
Simon Thörnqvist <simon@plug-n-trade.com>
2018-10-14 20:54:47 +0200

    switch evil commentary key

--- +bindings.el
+++ +bindings.el
@@ -1,5 +1,7 @@
 ;;; ~/.doom.d/+bindings.el -*- lexical-binding: t; -*-
 
-(map! :leader
-      :desc "Terminal in popup" :n "'" #'+term/open-popup-in-project
-      )
+(map!
+ (:leader
+   :desc "Ex command" :nv "!"  #'evil-ex
+   :desc "Terminal in popup" :n "'" #'+term/open-popup-in-project
+   :desc "[un]comment" :n  ";"  #'evil-commentary))
