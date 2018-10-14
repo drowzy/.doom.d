1da045522a028494c28f9f39f6bab04ccdd3049d
Simon Thörnqvist <simon@plug-n-trade.com>
2018-10-14 19:44:30 +0200

    begin custom keybindings

---
+++ +bindings.el
@@ -0,0 +1,5 @@
+;;; ~/.doom.d/+bindings.el -*- lexical-binding: t; -*-
+
+(map! :leader
+      :desc "Terminal in popup" :n "'" #'+term/open-popup-in-project
+      )
---
+++ config.el
@@ -0,0 +1,13 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+;;
+(load! "+bindings")
+
+(setq-default
+ user-full-name "Simon Thörnqvist"
+ user-mail-address "simon@plug-n-trade.com"
+
+ doom-font (font-spec :family "Sauce Code Powerline" :size 14)
+ doom-big-font (font-spec :family "Sauce Code Powerline" :size 16)
+ )
