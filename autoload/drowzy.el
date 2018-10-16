6d72f8e83b9bdd077219d92aafca223a1d61efd2
Simon Thörnqvist <simon@plug-n-trade.com>
2018-10-16 10:23:38 +0200

    add url-encode/decode-region functions

--- autoload/drowzy.el
+++ autoload/drowzy.el
@@ -5,3 +5,17 @@
   "Toggles between current/previous buffer."
   (interactive)
   (switch-to-buffer (other-buffer (current-buffer) 1)))
+
+;;;###autoload
+(defun +drowzy/url-encode-region (start end)
+  (interactive "r")
+  (let ((text (url-hexify-string (buffer-substring-no-properties start end))))
+    (delete-region start end)
+    (insert text)))
+
+;;;###autoload
+(defun +drowzy/url-decode-region (start end)
+  (interactive "r")
+  (let ((text (url-unhex-string (buffer-substring-no-properties start end))))
+    (delete-region start end)
+    (insert text)))
