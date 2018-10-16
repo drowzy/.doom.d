142ac87e3ef7e5e8b456f0f550e64557f9df7d5d
Simon Thörnqvist <simon@plug-n-trade.com>
2018-10-16 10:23:59 +0200

    add window, buffer bindings

--- +bindings.el
+++ +bindings.el
@@ -5,4 +5,19 @@
    :desc "Ex command" :nv "!"  #'evil-ex
    :desc "Terminal in popup" :n "'" #'+term/open-popup-in-project
    :desc "Toggle buffer" :n "`" #'+drowzy/switch-to-previous-buffer
-   :desc "[un]comment" :n  ";"  #'evil-commentary))
+   :desc "[un]comment" :n  ";"  #'evil-commentary
+
+   (:desc "window" :prefix "w"
+     :n "/" #'evil-window-vsplit
+     :n "_" #'evil-window-split
+     :n "d" #'+workspace/close-window-or-workspace)
+
+   (:desc "buffer" :prefix "b"
+     :desc "List buffers" :n "b" #'ivy-switch-buffer
+     :desc "Close buffer" :n "d" #'kill-this-buffer)
+   (:desc "project" :prefix "p"
+     :desc "Switch project"                :n  "p" #'projectile-switch-project
+     (:when (featurep! :ui neotree)
+       :desc "Project sidebar"              :n  "t" #'+neotree/open)
+     (:when (featurep! :ui treemacs)
+       :desc "Project sidebar"              :n  "t" #'+treemacs/toggle))))
